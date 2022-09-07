87591570c2dc56acee722eee0275ff1cd9bd9a02
juliabulatova <tikhomirovajulia1002@gmail.com>
2022-09-07 14:19:49 +0300

    6.9. String - это массив [#504884 #340427]

--- src/main/java/ru/job4j/array/JavaNameValidator.java
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -3,16 +3,14 @@ package ru.job4j.array;
 public class JavaNameValidator {
 
     public static boolean isNameValid(String name) {
-        if (name.isEmpty()) {
+        if (name.isEmpty() || !isLowerLatinLetter(name.codePointAt(0))) {
             return false;
         }
 
-        for (int i = 0; i < name.length(); i++) {
+        for (int i = 1; i < name.length(); i++) {
             int code = name.codePointAt(i);
 
-            if (i == 0 && !isLowerLatinLetter(code)) {
-                return false;
-            } else if (!isSpecialSymbol(code)
+            if (!isSpecialSymbol(code)
                     && !isUpperLatinLetter(code)
                     && !isLowerLatinLetter(code)
                     && !Character.isDigit(code)) {
